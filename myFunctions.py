94c7ddab1aacb2043ddf3b842defacb802dcde8e
karste1407 <karste1407@gmail.com>
2020-02-18 08:01:53 +0100

    added some files2

--- myFunctions.py
+++ myFunctions.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 def fun(x,i):
     beta = 0.2
     gamma = 0.1
@@ -10,11 +13,45 @@ def fun(x,i):
 
     return beta*(x_t)/(1+x_t**n) - gamma*x[i]
 
-def EulerForward(x_init,x_end,h):
-    N = (x_end-x_init)/h
-    x = np.zeros((1,N+1))
+def EulerForward(x_init,t_init,t_end,h):
+    N = int((t_end-t_init)/h)
+    t = np.linspace(t_init,t_end,N)
+    x = np.zeros((N,))
     x[0] = x_init
-    y[1] = y_init
-    for i in range(1,N):
+    for i in range(N-1):
         x[i+1] = x[i]+fun(x,i)
-    return x
+    return t,x
+
+def generateData(verbose=0):
+    x0 = 1.5
+    t_init = 300
+    t_end = 1200
+    step = 1
+    t,x = EulerForward(x0,t_init,t_end,step)
+
+    if verbose:
+        plt.plot(t,x,'b',label='EulerForward')
+        plt.xlabel('t')
+        plt.ylabel('x(t)')
+        plt.title('Mackey-Glass TimeSeries')
+        plt.show()
+
+    X_data = np.zeros((5,x.shape[0]))
+    shifts = [20,15,10,5,0]
+    for t in range(x.shape[0],0):
+        for d,shift in enumerate(shifts):
+            try:
+                X_data[d,t] = x[t-shift]
+            except IndexError:
+                pass
+    return X_data
+
+def generateSubsets(X,nTest,trVal_split):
+    X_tmp = X[:,:X.shape[1]-nTest-1]
+    X_test = X[:,X.shape[1]-nTest:-1]
+
+    n_train = int(trVal_split*X_tmp.shape[1])
+    X_train = X_tmp[:,n_train]
+    X_val = X_tmp[:,n_train+1:-1]
+
+    return X_train,X_val,X_test
--- myTest.py
+++ myTest.py
@@ -1,15 +1,34 @@
-def generateData():
-    from myFunctions import EulerForward
-    x0 = 1.5
-    t_init = 300
-    t_end = 1500
-    x = EulerForward(x0,t-init,t_end,step)
-    return x
-
+from myFunctions import *
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
 
 def main():
-    x = generateData():
-    print(x)
+
+    nTest = 200          #Number of samples for test data
+    split = 0.8          #Ration of training compared to validation
+
+    X = generateData()
+    X_train,X_val,X_test = generateSubsets(X,nTest,split)
+
+    '''
+    nPerceptrons = 8
+    nOut = 1
+
+    inputs = keras.Input(shape=(X_train.shape[0],), name='time-shifts')
+    x = layers.Dense(nPerceptrons, activation='sigmoid', name='dense_1')(inputs)
+    x = layers.Dense(nPerceptrons, activation='sigmoid', name='dense_2')(x)
+    outputs = layers.Dense(nOut, activation='sigmoid', name='predictions')(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+
+    model.compile(optimizer=keras.optimizers.SGD(),
+        loss=keras.losses.MeanSquaredError(),
+        metrics=[keras.metrics.CategoricalAccuracy()]
+        )
+    '''
+
+
+
+
 
 if __name__ == "__main__":
     main()
